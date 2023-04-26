3aa16a2424c98059de0425d41e4a62c700c64df7
MatheusRisatti <risattimatheus@gmail.com>
2023-04-26 19:00:08 -0300

    cria a navegação

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom";
+
 export function Cart(){
     return(
+        <>
         <h1>Cart</h1>
+        <Link to="/">Página inicial</Link>
+        
+        </>
     )
 }
